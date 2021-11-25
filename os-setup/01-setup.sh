fb9d639fc41397d3a918bfb6cd2627e07987a2e0
Ivo Donchev <i.donchev@hacksoft.io>
2021-11-25 11:52:45 +0200

    Add xclip

--- os-setup/01-setup.sh
+++ os-setup/01-setup.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y bat
 sudo apt-get install -y tldr
 sudo apt-get install -y curl
 sudo apt-get install -y silversearcher-ag
-
+sudo apt install -y xclip
 
 # Zsh and Oh-My_Zsh
 sudo apt-get install -y zsh
