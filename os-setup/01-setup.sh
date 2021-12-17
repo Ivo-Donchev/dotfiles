d4fec3dc00fd9a879d009152079323aa50cc9213
Ivo Donchev <i.donchev@hacksoft.io>
2021-12-17 11:07:00 +0200

    Add -y

--- os-setup/01-setup.sh
+++ os-setup/01-setup.sh
@@ -46,8 +46,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
 # Pyenv
 sudo apt install -y python3-pip
-sudo pip3 install virtualenv
-sudo apt-get install libpq-dev python-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libffi-dev
+sudo pip3 install -y virtualenv
+sudo apt-get install -y libpq-dev python-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libffi-dev
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
   libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
