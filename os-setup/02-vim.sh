f2ea02967f01415781fe4c793c41bf93639d9073
Ivo Donchev <i.donchev@hacksoft.io>
2021-12-22 16:32:10 +0200

    Finish vim setup

--- os-setup/02-vim.sh
+++ os-setup/02-vim.sh
@@ -1,4 +1,15 @@
 sudo apt-get install -y vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Main configuration
 cp ../vimrc ~/.vimrc
+
+# Setup color scheme
+mkdir -p ~/.vim/colors
+cp ../gruvbox.vim ~/.vim/colors
+
+# Setup pathogen
+mkdir -p ~/.vim/autoload
+touch ~/.vim/autoload/pathogen.vim
+curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim > ~/.vim/autoload/pathogen.vim
